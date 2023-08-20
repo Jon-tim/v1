02a33d932275fc326ea3bea64e594b531f8bffdd
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-20 03:49:45 +0100

    add audio file and remove unused files

--- src/components/home/Hero-Grid.tsx
+++ src/components/home/Hero-Grid.tsx
@@ -8,13 +8,13 @@ export default function HeroGrid() {
 					<div className="flex items-center group h-[40%] gap-4 overflow-hidden">
 						<div className="box p- bg-paleBlue h-full w-1/2 flex-1 flex items-center justify-center  overflow-hidden relative">
 							<i className="bx bx-briefcase text-7xl text-dark group-hover:-translate-y-[150%] transition-all duration-700 delay-200"></i>
-							<p className="absolute transition-all duration-700 translate-y-[300%]  group-hover:-translate-y-[0%] text-dark font-semibold text-3xl capitalize">
+							<p className="absolute transition-all duration-700 translate-y-[260%]  group-hover:-translate-y-[0%] text-dark font-semibold text-3xl capitalize">
 								projects
 							</p>
 						</div>
 						<div className="box p- h-full bg-paleRed flex-1 flex w-1/2 items-center justify-center  overflow-hidden relative">
-							<i className="bx bx-briefcase text-7xl text-dark transition-all duration-700 -translate-y-[300%]  group-hover:translate-y-[0%]"></i>
-							<p className="absolute group-hover:translate-y-[300%] transition-all duration-700 delay-300  text-dark font-semibold text-3xl capitalize">
+							<i className="bx bx-briefcase text-7xl text-dark transition-all duration-700 -translate-y-[150%]  group-hover:translate-y-[0%]"></i>
+							<p className="absolute group-hover:translate-y-[260%] transition-all duration-700 delay-200  text-dark font-semibold text-3xl capitalize">
 								projects
 							</p>
 						</div>
@@ -29,7 +29,7 @@ export default function HeroGrid() {
 				<div className="md:w-[45%] child-box h-[400px] w-full box box-2 bg-paleGreen p-5 relative overflow-hidden">
 					<div className=" h-full w-full text-center flex flex-col gap-3 text-dark">
 						<p className="text-xl font-semibold">
-							I am interested in the roles and application of IoTs
+							I am interested in the application of IoTs
 							in the fields of Education and Agriculture in Africa
 						</p>
 					</div>
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,15 @@
+import { useRef } from "react";
 import Footer from "./Footer";
 import HeroGrid from "./Hero-Grid";
 
 export default function Hero() {
+	const audioRef = useRef<HTMLAudioElement>(null);
+
+	const playAudio = () => {
+		if (audioRef.current) {
+			audioRef.current.play();
+		}
+	};
 	return (
 		<section className="mt-12 md:mt-16 flex flex-col gap-10">
 			<div className="text-center flex flex-col items-center max-w-2xl mx-auto gap-4">
@@ -18,17 +26,29 @@ export default function Hero() {
 					</h1>
 					<p>
 						<span className="opacity-80">I am</span>{" "}
-						<span className="bg-paleBrown px-3 rounded-full font-semibold inline-flex items-center gap-2 w-max cursor-pointer text-dark opacity-100 capitalize">
+						<span
+							onClick={playAudio}
+							className="bg-paleBrown px-3 rounded-full font-semibold inline-flex items-center gap-2 w-max cursor-pointer text-dark opacity-100 capitalize"
+						>
 							oluwatimilehin awoniyi
 							<i className="bx bxs-volume-full"></i>
 						</span>{" "}
+						<audio
+							ref={audioRef}
+							className="hidden"
+						>
+							<source
+								src="/src/assets/name.mp3"
+								type="audio/mp3"
+							/>
+							Your browser does not support the audio element.
+						</audio>
 						<span className="opacity-80">
-							a Frontend Developer proficient with React
-							and Svelte to create responsive UI components and
-							optimizing performance for handling large datasets.
-							Most times, I use water, keystrokes and algorithms
-							to solve problems. Other times, I use code to build
-							bridges between people and technology.
+							a Frontend Developer proficient in JavaScript (React
+							and Svelte). I specialize in creating responsive UI
+							components and using code to connect people and
+							technology. I apply logical thinking, algorithms,
+							and creativity to problem-solving.
 						</span>
 					</p>
 				</div>
